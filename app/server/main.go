cc13750d08835556b7fd953add9ace380c87f4ac
Argana Phangquestian <arganaphangquestian@gmail.com>
2022-03-04 17:02:20 +0700

    remove: server boilerplate

--- app/server/main.go
+++ app/server/main.go
@@ -2,50 +2,8 @@ package main
 
 import (
 	"fmt"
-	"os"
-
-	tea "github.com/charmbracelet/bubbletea"
 )
 
-type model struct {
-	count int
-}
-
-func newModel() model {
-	return model{
-		count: 0,
-	}
-}
-
 func main() {
-	p := tea.NewProgram(newModel())
-	if err := p.Start(); err != nil {
-		fmt.Printf("Alas, there's been an error: %v", err)
-		os.Exit(1)
-	}
-}
-
-func (m model) Init() tea.Cmd {
-	return nil
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "q":
-			return m, tea.Quit
-		case "up", "k":
-			m.count++
-		case "down", "j":
-			m.count--
-		}
-	}
-	return m, nil
-}
-
-func (m model) View() string {
-	s := "Total Count?\n\n"
-	s += "\nPress `q` or `ctrl+c` to quit.\n"
-	return s
+	fmt.Println("Hello from server")
 }
